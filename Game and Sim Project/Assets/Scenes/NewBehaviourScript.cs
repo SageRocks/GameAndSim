fa3619fe4aabb29fc480955804257dc23dc53b20
SageRocks <43547590+SageRocks@users.noreply.github.com>
2018-09-24 13:01:35 -0400

    uploaded unity project

---
+++ Game and Sim Project/Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public Light myLight;
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown ("Space"))
+        {
+            myLight.enabled = true;
+        }
+        if(Input.GetKeyUp ("Space"))
+        {
+            myLight.enabled = false;
+        }
+	}
+}
