396c4576ff6b2c7313a2780acf2fa26c6b40c39f
SageRocks <43547590+SageRocks@users.noreply.github.com>
2018-10-09 13:13:57 -0400

    moving backdrop

---
+++ Game and Sim Project/Assets/backdrop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backdrop : MonoBehaviour {
+
+    Vector2 tempPos;
+
+	// Use this for initialization
+	void Start () {
+        tempPos = new Vector2 (5f, 6f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        /*tempPos = transform.position;
+        tempPos.x += 5f;
+        transform.position = tempPos;*/
+        tempPos = new Vector2(tempPos.x + 1, 6f);
+        Debug.Log(tempPos.x);
+    }
+}
