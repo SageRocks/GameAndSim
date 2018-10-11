656224ae58d073965f77283abf8c9a61ab85a9e8
SageRocks <43547590+SageRocks@users.noreply.github.com>
2018-10-11 17:41:21 -0400

    kub

--- Game and Sim Project/Assets/backdropBehavior.cs
+++ Game and Sim Project/Assets/backdropBehavior.cs
@@ -3,18 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class backdropBehavior : MonoBehaviour {
-    Vector3 tempPos;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        //Vector3 tempPos;
-        tempPos = transform.position;
-        tempPos.x -= 0.002f;
-        if (tempPos.x < -44.1537) tempPos.x = -10.0537f;
+        Vector3 tempPos;
+        tempPos = transform.position;tempPos.x -= 3f;
+        if(tempPos.x < -3440) tempPos.x = 3389;
         transform.position = tempPos;
     }
 }
